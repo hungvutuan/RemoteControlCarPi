511974785f097bf7fe25c7242ba06b7fdc637e54
Vu Tuan Hung <46418698+hungvutuan@users.noreply.github.com>
2019-10-03 22:57:39 +0700

    Final edit
    Delete url_for('forward')

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
-from importlib import import_module
+# from importlib import import_module
+# import os
+# from importlib import import_module
 from flask import Flask, render_template, Response
-import os
-from importlib import import_module
 
 
 # import camera driver
-if os.environ.get('CAMERA'):
-    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
-else:
-    from camera import Camera
+# if os.environ.get('CAMERA'):
+#     Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+# else:
+#     from camera import Camera
 
 # Raspberry Pi camera module (requires picamera package)
-# from camera_pi import Camera
+from camera_pi import Camera
 
 
 app = Flask(__name__)
